a709e81883c8283218ada99b11a35f8e2578ccdd
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-05-18 00:37:00 -0400

    chore: Add DayButton component for displaying daily deals on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { bodoni } from "@/lib/fonts";
 
 import { Button } from "@/components/ui/button"
 import Image from "next/image";
-import Link from "next/link";
 import { ArrowRight } from "lucide-react"
+
+import { DayButton } from "./_root_components/DayButton";
 
 export default function Home() {
   return (
@@ -108,27 +109,4 @@ export default function Home() {
       </section>
     </div>
   );
-}
-
-interface DayButtonProps {
-  right?: boolean;
-  title: string;
-  day: string;
-  discount: string;
-  href: string;
-  children?: React.ReactNode;
-}
-
-export function DayButton({ right, title, day, discount, children, href }: DayButtonProps) {
-  return (
-    <div className={`w-full flex ${right ? 'justify-end' : 'justify-start'}`}>
-      <Link href={href} className="day-link flex justify-between bg-white w-[70%] rounded-md overflow-hidden hover:bg-secondary animate-fade-in-up duration-600">
-        <div className="px-3 py-6">
-          <h3 className="transition-all duration-300 text-primary font-semibold text-2xl">{title}</h3>
-          <strong className="transition-all duration-300 text-lg text-accent font-semibold">{day} <span>{discount}</span></strong>
-        </div>
-        {children}
-      </Link>
-    </div>
-  )
 }
